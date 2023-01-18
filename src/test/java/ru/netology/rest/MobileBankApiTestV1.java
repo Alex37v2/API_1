9ecf3c6a6e70ed4d39150ebc391ea1b7e63b2995
Алексей Александров <Alex37v2@gmail.com>
2023-01-18 12:00:52 +1000

    Healthy spelling

--- src/test/java/ru/netology/rest/MobileBankApiTestV1.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV1.java
@@ -7,7 +7,6 @@ import static io.restassured.RestAssured.given;
 class MobileBankApiTestV1 {
     @Test
     void shouldReturnDemoAccounts() {
-
         given()
                 .baseUri("http://localhost:9999/api/v1")
                 .when()
